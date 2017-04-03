38b50a9103eced6e5ba00a592c9be0b86e1236d5
spalonytoster <xmachiei@gmail.com>
2017-04-03 15:22:06 +0200

    $.remove example

--- src/main/java/com/mposluszny/lodash/FilterCollection.java
+++ src/main/java/com/mposluszny/lodash/FilterCollection.java
@@ -1,6 +1,8 @@
 package com.mposluszny.lodash;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import com.github.underscore.lodash.$;
 
@@ -14,6 +16,21 @@ public class FilterCollection {
 		
 		$.reject(Arrays.asList(1, 2, 3), (el) -> el == 2)
 		 .forEach((el) -> System.out.println(el));
+
+		System.out.println("\n--------------------\n");
+
+		/*
+		 * Arrays.asList returns its own implementation of ArrayList which is implemented as a view
+		 * of the created array. It does not implement some methods eg. `remove` (does so because
+		 * elements of given array won't have to be copied - they're just being wrapped).
+		 */
+		List<Integer> arr = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+		List<?> evens = $.remove(arr, (el) -> el % 2 == 0);
+		List<?> odds = arr;
+		
+		System.out.println("evens: " + evens);
+		System.out.println("odds: " + odds);
+		
 	}
 
 }
